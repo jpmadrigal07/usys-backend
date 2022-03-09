072c998f2e7efaa49518a08a062a0a810a01685e
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-03-09 21:01:59 +0800

    Changes for 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.1
+
+- Change configs for DB Connections
 ## 0.0.0
 
 - Change README
--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,9 @@
 // KEYS.JS
-const envStage = process.env.REACT_APP_STAGE;
-const currentStage = envStage ? envStage.trim() : "local";
 
-if (currentStage === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // PRODUCTIONS KEYS
     module.exports = require('./production');
-} else if (currentStage === 'development') {
+} else {
     // DEV KEYS
     module.exports = require('./development');
-} else if (currentStage === 'local') {
-    // DEV KEYS
-    module.exports = require('./local');
 }
