821453bb953a6b274af3215411b18c9e59f55f64
marcsteven01 <marcesteban48@gmail.com>
2022-03-12 19:06:13 +0800

    0006-Create Student Type CRUD (College Level, SHS Level)

--- routes/studentType.js
+++ routes/studentType.js
@@ -8,18 +8,86 @@ const isEmpty = require("lodash/isEmpty");
 // @desc    Get All studentType
 // @access  Public
 router.get("/", async (req, res) => {
-    const condition = !isNil(req.query.condition)
-      ? JSON.parse(req.query.condition)
-      : {};
-    if (isNil(condition.deletedAt)) {
-      condition.deletedAt = {
-        $exists: false,
-      };
-    }
+  const condition = !isNil(req.query.condition)
+    ? JSON.parse(req.query.condition)
+    : {};
+  if (isNil(condition.deletedAt)) {
+    condition.deletedAt = {
+      $exists: false,
+    };
+  }
+  try {
+    const getAllStudentType = await StudentType.find(condition);
+    res.json({
+      dbRes: getAllStudentType,
+      isSuccess: true,
+    });
+  } catch (error) {
+    res.json({
+      dbRes: error.message,
+      isSuccess: false,
+    });
+  }
+});
+
+// @route   POST api/studentType/add
+// @desc    Add A studentType
+// @access  Private
+router.post("/", async (req, res) => {
+  const type = req.body.type;
+  if (!isNil(type)) {
+    const newStudentType = new StudentType({
+      type,
+    });
     try {
-      const getAllStudentType = await StudentType.find(condition);
+      const getStudentType = await StudentType.find({
+        type,
+        deletedAt: {
+          $exists: false,
+        },
+      });
+      if (getStudentType.length === 0) {
+        const createStudentType = await newStudentType.save();
+        res.json({
+          dbRes: createStudentType,
+          isSuccess: true,
+        });
+      } else {
+        res.json({
+          dbRes: "Student Type is already in use",
+          isSuccess: false,
+        });
+      }
+    } catch (error) {
       res.json({
-        dbRes: getAllStudentType,
+        dbRes: error.message,
+        isSuccess: false,
+      });
+    }
+  } else {
+    res.json({
+      dbRes: "Required values are either invalid or empty",
+      isSuccess: false,
+    });
+  }
+});
+
+// @route   PATCH api/StudentType/:id
+// @desc    Update A StudentType
+// @access  Private
+router.patch("/:id", async (req, res) => {
+  const condition = req.body;
+  if (!isEmpty(condition)) {
+    try {
+      const updateStudentType = await StudentType.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: condition,
+          updatedAt: Date.now(),
+        }
+      );
+      res.json({
+        dbRes: updateStudentType,
         isSuccess: true,
       });
     } catch (error) {
@@ -28,115 +96,50 @@ router.get("/", async (req, res) => {
         isSuccess: false,
       });
     }
-  });
-
-// @route   POST api/studentType/add
-// @desc    Add A studentType
-// @access  Private
-router.post("/", async (req, res) => {
-    const type = req.body.type;
-    const level = req.body.level;
-    if (!isNil(type) && !isNil(level)) {
-      const newStudentType = new StudentType({
-        type,
-        level
-      });
-      try {
-        const getStudentType = await StudentType.find({
-            type,
-            level,
-          deletedAt: {
-            $exists: false,
-          },
-        });
-        if (getStudentType.length === 0) {
-          const createStudentType = await newStudentType.save();
-          res.json({
-            dbRes: createStudentType,
-            isSuccess: true,
-          });
-        } else {
-          res.json({
-            dbRes: "Student Level is already in use",
-            isSuccess: false,
-          });
-        }
-      } catch (error) {
-        res.json({
-          dbRes: error.message,
-          isSuccess: false,
-        });
-      }
-    } else {
-      res.json({
-        dbRes: "Required values are either invalid or empty",
-        isSuccess: false,
-      });
-    }
-  });
-
-// @route   PATCH api/StudentType/:id
-// @desc    Update A StudentType
-// @access  Private
-router.patch("/:id", async (req, res) => {
-    const condition = req.body;
-    if (!isEmpty(condition)) {
-      try {
-        const updateStudentType = await StudentType.findByIdAndUpdate(req.params.id, {
-          $set: condition,
-          updatedAt: Date.now(),
-        });
-        res.json({
-          dbRes: updateStudentType,
-          isSuccess: true,
-        });
-      } catch (error) {
-        res.json({
-          dbRes: error.message,
-          isSuccess: false,
-        });
-      }
-    } else {
-      res.json({
-        dbRes: "Student Level Cannot be found",
-        isSuccess: false,
-      });
-    }
-  });
+  } else {
+    res.json({
+      dbRes: "Student Type Cannot be found",
+      isSuccess: false,
+    });
+  }
+});
 
 // @route   DELETE api/StudentType/:id
 // @desc    Delete A StudentType
 // @access  Private
 router.delete("/:id", async (req, res) => {
-    try {
-      const getStudentType = await StudentType.find({
-        _id: req.params.id,
-        deletedAt: {
-          $exists: false,
-        },
-      });
-      if (getStudentType.length > 0) {
-        const deleteStudentType = await StudentType.findByIdAndUpdate(req.params.id, {
+  try {
+    const getStudentType = await StudentType.find({
+      _id: req.params.id,
+      deletedAt: {
+        $exists: false,
+      },
+    });
+    if (getStudentType.length > 0) {
+      const deleteStudentType = await StudentType.findByIdAndUpdate(
+        req.params.id,
+        {
           $set: {
             deletedAt: Date.now(),
           },
-        });
-        res.json({
-          dbRes: deleteStudentType,
-          isSuccess: true,
-        });
-      } else {
-        res.json({
-          dbRes: "Student Level is already deleted",
-          isSuccess: false,
-        });
-      }
-    } catch (error) {
+        }
+      );
       res.json({
-        dbRes: error.message,
+        dbRes: deleteStudentType,
+        isSuccess: true,
+      });
+    } else {
+      res.json({
+        dbRes: "Student Type is already deleted",
         isSuccess: false,
       });
     }
-  });
-  
-  module.exports = router;
+  } catch (error) {
+    res.json({
+      dbRes: error.message,
+      isSuccess: false,
+    });
+  }
+});
+
+module.exports = router;
